8e484289decd3c0f1b3642be8be25d496d4821be
vicky <lch82327@gmail.com>
2017-03-26 03:56:55 +0800

    config file

---
+++ config/environments.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+configure :development do
+  ENV['DATABASE_URL'] = 'sqlite://db/dev.db'
+  require 'hirb'
+  Hirb.enable
+end
+
+configure :test do
+  ENV['DATABASE_URL'] = 'sqlite://db/test.db'
+end
+
+configure :development, :test do
+  # both development and test database use the same key
+  require 'config_env'
+  ConfigEnv.path_to_config("#{__dir__}/config_env.rb")
+end
+
+configure do
+  enable :logging
+  require 'sequel'
+  DB = Sequel.connect(ENV['DATABASE_URL'])
+end
